d501fd8bf27fa161700ee430b8bf6ebe85d31338
Shaker Sharabi <sheko100@icloud.com>
2023-08-08 03:59:13 +0300

    Add check_cycle.c file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if the linked list has a cycle
+ * @list: pointer to a linked list
+ *
+ * Return: 1 if it has a cycle
+ * 0 otherwise
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *nodep;
+
+	nodep = list;
+	while (nodep != NULL)
+	{
+		if (nodep->next == list)
+			return (1);
+		nodep = nodep->next;
+	}
+	return (0);
+}
