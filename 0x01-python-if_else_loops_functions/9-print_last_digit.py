e2a0b3f313a51edd4a74d4710988933e3279c2af
Shaker Sharabi <sheko100@icloud.com>
2023-08-09 03:28:54 +0300

    Add print_last_digit.py file

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,5 @@
+def print_last_digit(number):
+    """prints the last digit"""
+    lastdigit = -(number % -10) if (number < 0) else number % 10
+    print("{}".format(lastdigit), end="")
+    return (lastdigit)
