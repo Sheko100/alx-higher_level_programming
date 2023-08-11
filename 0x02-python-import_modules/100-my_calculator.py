84b75b6f1aa9ef0d8d7e6accac90b12bbf174252
Shaker Sharabi <sheko100@icloud.com>
2023-08-12 02:21:55 +0300

    Add my_calculator.py file

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    arg = sys.argv
+    arglen = len(arg)
+    operr = "Unknown operator. Available operators: +, -, * and /"
+    res = 0
+    if (arglen < 4):
+        print("Usage: {} <a> <operator> <b>".format(arg[0]), file=sys.stderr)
+        sys.exit(1)
+    match arg[2]:
+        case "+":
+            res = add(int(arg[1]), int(arg[3]))
+        case "-":
+            res = sub(int(arg[1]), int(arg[3]))
+        case "*":
+            res = mul(int(arg[1]), int(arg[3]))
+        case "/":
+            res = div(int(arg[1]), int(arg[3]))
+        case _:
+            print("{}".format(operr), file=sys.stderr)
+            sys.exit(1)
+    print("{} {} {} = {}".format(arg[1], arg[2], arg[3], res))
