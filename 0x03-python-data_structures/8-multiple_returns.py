d352bfa05af51eee819bf5adab86072de8e87daf
Shaker Sharabi <sheko100@icloud.com>
2023-08-13 20:58:05 +0300

    Add multiple_returns.py file

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    strlen = len(sentence)
+    firstchar = sentence[0] if (strlen > 0) else None
+    
+    strobj = strlen, firstchar
+    return (strobj)
