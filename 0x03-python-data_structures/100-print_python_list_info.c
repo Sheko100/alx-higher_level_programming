96799694b3c0d1285a1b7e67d320528a6bee9f30
Shaker Sharabi <sheko100@icloud.com>
2023-08-15 05:40:27 +0300

    Add print_python_list_info.c file

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_python_list_info - prints info about python list object
+ * @PyObject: pointer to a python object
+ *
+ * Return: void always
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	printf("");
+}
+
+/**
+ * main - entry point
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+
+	return (0);
+}
