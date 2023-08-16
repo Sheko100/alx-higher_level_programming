72799ab3d174c5309bd7710a0c86451257248cd8
Shaker Sharabi <sheko100@icloud.com>
2023-08-17 02:55:17 +0300

    Fix None case

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     best = 0
     name = ""
-    if (a_dictionary == None):
+    if (a_dictionary is None):
         return (None)
     for k, v in a_dictionary.items():
         if (v > best):
