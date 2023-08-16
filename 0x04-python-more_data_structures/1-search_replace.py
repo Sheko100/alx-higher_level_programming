b67ebb09813b428884fa2224a6a35e4f35e296ab
Shaker Sharabi <sheko100@icloud.com>
2023-08-17 01:58:07 +0300

    Fix list comprehension

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    isthenum = lambda num, search, replace: replace if num == search else num
-    new_list = [isthenum(n, search, replace) for n in my_list]
+    new_list = [n if n != search else replace for n in my_list]
     return (new_list)
