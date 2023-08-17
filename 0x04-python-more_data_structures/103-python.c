d2ffb31779bd2a90cc56028e10cd88ad851ac3ff
Shaker Sharabi <sheko100@icloud.com>
2023-08-17 04:59:29 +0300

    Add python.c file

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_python_list - prints info about python list
+ * @p: pointer to a python object
+ *
+ * Return: void
+ */
+void print_python_list(PyObject *p)
+{
+
+}
+
+/**
+ * print_python_bytes - prints info about python bytes
+ * @p: pointer to a python object
+ *
+ * Return: void
+ */
