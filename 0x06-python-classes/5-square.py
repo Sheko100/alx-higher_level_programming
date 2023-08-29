c5ede718d1aad61df9cb899036d63ccf05b08f6f
Shaker Sharabi <sheko100@icloud.com>
2023-08-29 20:23:32 +0300

    Add 5-square.py file

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Class Square
+
+    This module creates a class called Square
+"""
+
+
+class Square:
+    """a class defines a Square with size
+
+    Args:
+        size (int): The size of the square
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """int: gets and sets the size of the square
+
+        Raises a TypeError if the size is not integer and
+        raises a ValueError if the size less than 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Gets the square area
+
+        Returns: the square area
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square with #"""
+        row = 0
+        size = self.__size
+        if size > 0:
+            while (row < size):
+                col = 0
+                while (col < size):
+                    print("#", end="")
+                    col += 1
+                print()
+                row += 1
+        else:
+            print()
