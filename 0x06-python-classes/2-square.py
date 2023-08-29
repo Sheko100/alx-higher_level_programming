e7a23eef00ed196b8a049dfdf83e0e7615d85ddc
Shaker Sharabi <sheko100@icloud.com>
2023-08-29 19:01:43 +0300

    Add 2-square.py file

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Class Square
+
+    This module creates a class called Square
+"""
+
+
+class Square:
+    """a class defines a Square with size
+
+    Args:
+        size (int): The size of the square
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
