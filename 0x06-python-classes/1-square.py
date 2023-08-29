a203892e385f9d2cdc080709f4ca7429017728f1
Shaker Sharabi <sheko100@icloud.com>
2023-08-29 18:46:54 +0300

    Add 1-square.c file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Class Square
+
+    This module creates a class called Square
+"""
+
+
+class Square:
+    """Class that defines a sqaure
+
+    Args:
+        size (int): the size of the square
+    """
+    def __init__(self, size):
+        self.__size = size
