f380d3a493d2179c642c553afa161b3325aa5170
Shaker Sharabi <sheko100@icloud.com>
2023-08-29 18:26:41 +0300

    Add more docstrings

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Creating a class that defines a Square"""
+
+
 class Square:
-    """A class that defines a sqaure
-    """
+    """A class that defines a sqaure"""
