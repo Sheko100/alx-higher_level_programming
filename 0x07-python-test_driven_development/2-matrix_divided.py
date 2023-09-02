9202b83c3a49cbfb5e5adfc758181b4d7ba8aad4
Shaker Sharabi <sheko100@icloud.com>
2023-09-03 00:26:09 +0300

    Fix coding style

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Module to divide a matrix.
 """
+
+
 def matrix_divided(matrix, div):
     """divides all elements of a matrix
 
@@ -17,29 +19,30 @@ def matrix_divided(matrix, div):
                 if div is neither an int nor float.
             ZeroDivisionError: if div is zero
     """
+    err = "matrix must be a matrix (list of lists) of integers/floats"
     if div == 0:
         raise ZeroDivisionError("division by zero")
     elif not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(err)
     elif len(matrix) > 0 and isinstance(matrix[0], list):
         rowsize = len(matrix[0])
         if (rowsize < 1):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(err)
     else:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(err)
 
     newmatrix = []
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(err)
         elif len(row) < 1 or len(row) != rowsize:
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError(err)
         newlist = []
         for col in row:
             if not isinstance(col, int) and not isinstance(col, float):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(err)
             newlist.append(round(col / div, 2))
         newmatrix.append(newlist)
     return (newmatrix)
