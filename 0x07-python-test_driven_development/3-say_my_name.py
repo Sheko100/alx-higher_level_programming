857c02b47fed04846d2f8b0c0a721be4fcce9b40
Shaker Sharabi <sheko100@icloud.com>
2023-09-03 00:39:34 +0300

    Add docstring

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""Module to print a full name
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """ prints "My name is <first name> <last name>"
+
+    Args:
+        first_name (str): first name
+        last_name (str): last name
+
+    Raises:
+        TypeError: if first_name or last_name is not a string
+            if firs_name is empty
+            
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif len(first_name) < 1:
