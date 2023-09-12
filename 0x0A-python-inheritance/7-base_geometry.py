affc46535457e48b83f7ac00afa75b8ba65f44be
Shaker Sharabi <sheko100@icloud.com>
2023-09-12 10:52:43 +0300

    Fix no args case

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -11,7 +11,7 @@ class BaseGeometry:
         """
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
+    def integer_validator(self, name="value", value=""):
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
