1f28a83f30b91a2574cc1404a7d0cd248c370ab9
Shaker Sharabi <sheko100@icloud.com>
2023-09-13 11:07:44 +0300

    Fix empty list case

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -31,9 +31,10 @@ class Student:
             attr: attributes to include in the dictionary
         """
         dct = {}
-        if type(attrs) == list and len(attrs) > 0 and type(attrs[0]) == str:
-            for attr in attrs:
-                if attr in self. __dict__:
-                    dct[attr] = self.__dict__[attr]
+        if type(attrs) == list:
+            if len(attrs) > 0 and type(attrs[0]) == str:
+                for attr in attrs:
+                    if attr in self. __dict__:
+                        dct[attr] = self.__dict__[attr]
             return dct
         return self.__dict__
