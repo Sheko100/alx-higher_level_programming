382f76f22a5dd403a02d7563bc1028ed32d519ac
Shaker Sharabi <sheko100@icloud.com>
2023-09-13 12:13:35 +0300

    Fix add items case

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,11 +6,28 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-argv = sys.argv
-args = []
-for arg in argv[1:]:
-    args.append(arg)
 
-with open("add_item.json", mode="w", encoding="utf-8") as file:
+filename = "add_item.json"
+args = []
+argv = sys.argv
+if len(argv) > 1:
+    obj = load_from_json_file(filename)
+    for ele in obj:
+        args.append(ele)
+    for arg in argv[1:]:
+        args.append(arg)
+    save_to_json_file(args, filename)
+else:
+    save_to_json_file([], filename)
+"""jsons = file.readline()
+    print(len(jsons))
+    if len(jsons) > 0:
+        print(args)
+        obj = json.loads(jsons)
+        for ele in obj:
+            args.append(ele)
+    for arg in sys.argv[1:]:
+        args.append(arg)
     jsons = json.dumps(args)
     wrtten = file.write(jsons)
+"""
