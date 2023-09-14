a1795ad73f02439613d21b8b37ce46a0c66980d0
Shaker Sharabi <sheko100@icloud.com>
2023-09-14 12:01:24 +0300

    Add Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/pyhton3
+"""Module for defining the Base class
+"""
+
+
+class Base:
+    """class that is the base of all other classes
+
+    Attributes:
+        __nb_objects (int): classes count
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initializes an instance object of Base class
+
+        Args:
+            id (int): instance id
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
