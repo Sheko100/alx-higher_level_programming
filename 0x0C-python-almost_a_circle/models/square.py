350360d6387e90c617aa49d8a87b04694e23f7fe
Shaker Sharabi <sheko100@icloud.com>
2023-09-15 20:18:20 +0300

    Add update method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,7 +29,7 @@ class Square(Rectangle):
             TypeError: if the value is not int
             ValueError: if the value equals or less than zero
         """
-        return self.__width
+        return self.__size
 
     @size.setter
     def size(self, value):
@@ -41,9 +41,35 @@ class Square(Rectangle):
 
         self.__width = value
         self.__height = value
+        self.__size = value
 
     def __str__(self):
         name = self.__class__.__name__
         id, size, x, y = self.id, self.size, self.x, self.y
 
         return "[{}] ({}) {}/{} - {}".format(name, id, x, y, size)
+
+    def update(self, *args, **kwargs):
+        """Updates the square attributes
+
+        Args:
+            *args (tuple): argument list of attributes
+            **kwargs (dictionary): keyword argument list of attributes
+        """
+        if len(args) > 0:
+            argn = 0
+            for arg in args:
+                if argn == 0:
+                    self.id = arg
+                elif argn == 1:
+                    self.size = arg
+                elif argn == 2:
+                    self.x = arg
+                elif argn == 3:
+                    self.y = arg
+                argn += 1
+        else:
+            print(args)
+            for key in kwargs:
+                value = kwargs[key]
+                setattr(self, key, value)
