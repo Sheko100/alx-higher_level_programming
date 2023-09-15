d52b8e932e417da8891f7cc5750eb5e5f77b94bb
Shaker Sharabi <sheko100@icloud.com>
2023-09-15 09:44:03 +0300

    Add testing for the update method

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,8 +5,8 @@ import unittest
 from models.rectangle import Rectangle
 
 
-class TestRectangle(unittest.TestCase):
-    """Tests for the Rectangle class"""
+class TestRectangleWidth(unittest.TestCase):
+    """Tests for the width attributre of the Rectangle class"""
 
     def test_normal_width(self):
         """Test for the normal behavior of passing integer width"""
@@ -47,6 +47,9 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             rect3 = Rectangle(None, 2, 3, 1)
 
+class TestRectangleHeight(unittest.TestCase):
+    """Tests for the height attribute of the Rectangle class"""
+
     def test_normal_height(self):
         """Test for the normal behavior of passing integer height"""
 
@@ -86,6 +89,9 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             rect3 = Rectangle(5, None, 3, 1)
 
+class TestRectangleX(unittest.TestCase):
+    """Tests for the x axis attribute of the Rectangle class"""
+
     def test_normal_x_axis(self):
         """Test for the normal behavior of passing integer x"""
 
@@ -121,6 +127,9 @@ class TestRectangle(unittest.TestCase):
 
         with self.assertRaises(TypeError):
             rect3 = Rectangle(5, 2, None, 1)
+
+class TestRectangleY(unittest.TestCase):
+    """Tests for the y axis attribute of the Rectangle class"""
 
     def test_normal_y_axis(self):
         """Test for the normal behavior of passing integer y"""
@@ -158,6 +167,9 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             rect3 = Rectangle(5, 2, 3, None)
 
+class TestRectangleMethods(unittest.TestCase):
+    """Tests for the methods of the Rectangle class"""
+
     def test_area(self):
         """Test for the area output"""
 
@@ -170,3 +182,38 @@ class TestRectangle(unittest.TestCase):
         rect3 = Rectangle(5, 2, 3, 1, 55)
         self.assertEqual(str(rect3), "[Rectangle] (55) 3/1 - 5/2")
 
+    def test_update_rect(self):
+        """Test for the arguments list of update method"""
+
+        newid = 200
+        newwidth, newheight, newx, newy = 45, 30, 40, 20
+
+        rect3 = Rectangle(5, 2, 3, 1)
+
+        rect3.update(newid)
+        self.assertEqual(newid, rect3.id)
+
+        rect3.update(newid, newwidth)
+        self.assertEqual(newid, rect3.id)
+        self.assertEqual(newwidth, rect3.width)
+
+        rect3.update(newid, newwidth, newheight)
+        self.assertEqual(newid, rect3.id)
+        self.assertEqual(newwidth, rect3.width)
+        self.assertEqual(newheight, rect3.height)
+
+        rect3.update(newid, newwidth, newheight, newx)
+        self.assertEqual(newid, rect3.id)
+        self.assertEqual(newwidth, rect3.width)
+        self.assertEqual(newheight, rect3.height)
+        self.assertEqual(newx, rect3.x)
+
+        rect3.update(newid, newwidth, newheight, newx, newy)
+        self.assertEqual(newid, rect3.id)
+        self.assertEqual(newwidth, rect3.width)
+        self.assertEqual(newheight, rect3.height)
+        self.assertEqual(newx, rect3.x)
+        self.assertEqual(newy, rect3.y)
+
+if __name__ == '__main__':
+    unittest.main()
