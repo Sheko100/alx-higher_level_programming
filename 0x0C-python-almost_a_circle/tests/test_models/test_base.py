75d57751261fd7bfc274a1d9495bcd86d028f0ca
Shaker Sharabi <sheko100@icloud.com>
2023-09-16 11:12:44 +0300

    Add testing for to_json_string method

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -30,6 +30,49 @@ class TestBase(unittest.TestCase):
         self.assertEqual(obj55.id, 55)
         self.assertEqual(obj88.id, 88)
 
+    def test_to_json_string(self):
+        """Test for the to_json_string static method normal behavior"""
+
+        dictlist = [{"id": 55, "size": 10}, {"x": 5, "y": 4}, {"height": 7}]
+        obj4 = Base()
+
+        dictliststr = obj4.to_json_string(dictlist)
+        self.assertEqual(
+                dictliststr,
+                '[{"id": 55, "size": 10}, {"x": 5, "y": 4}, {"height": 7}]'
+        )
+
+    def test_to_json_string_empty_list(self):
+        """Test for the to_json_string static method with empty list"""
+
+        dictlist = []
+        obj4 = Base()
+
+        dictliststr = obj4.to_json_string(dictlist)
+        self.assertEqual(dictliststr,"[]")
+
+    def test_to_json_string_none_list(self):
+        """Test for passing None intead of list to
+        the to_json_string static method
+        """
+
+        obj4 = Base()
+        dictliststr = obj4.to_json_string(None)
+
+        self.assertEqual(dictliststr,"[]")
+
+    def test_to_json_string_empty_dict(self):
+        """Test for passing list of empty dictionaries to
+        the to_json_string static method
+        """
+
+        dictlist = [{}, {}, {}]
+        obj4 = Base()
+
+        dictliststr = obj4.to_json_string(dictlist)
+
+        self.assertEqual(dictliststr,"[{}, {}, {}]")
+
 
 if __name__ == '__main__':
     unittest.main()
