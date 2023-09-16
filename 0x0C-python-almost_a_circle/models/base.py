2b2b86a1e07484f51ac0bce2a79ec4f2696d5135
Shaker Sharabi <sheko100@icloud.com>
2023-09-16 11:13:45 +0300

    Add to_json_str method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/pyhton3
 """Module for defining the Base class
 """
+import json
 
 
 class Base:
@@ -22,3 +23,22 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Serializes the list of dictionaries to JSON
+
+        Args:
+            list_dictionaries (list): list of dictionaries
+
+        Returns:
+            str: JSON respresentation on success or 
+            empty list if list_dictionaries is empty or None
+        """
+
+        if list_dictionaries is None or len(list_dictionaries) < 1:
+            return "[]"
+
+        jsons = json.dumps(list_dictionaries)
+
+        return jsons
