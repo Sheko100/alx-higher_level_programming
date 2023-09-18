74ce6834f5534a14c2eaa3e51bde21aa6f41a5a7
Shaker Sharabi <sheko100@icloud.com>
2023-09-18 10:46:57 +0300

    Add models directory

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
 """
 import json
 import os
+import csv
+import turtle
+
 
 class Base:
     """class that is the base of all other classes
@@ -114,3 +117,82 @@ class Base:
                 objlist.append(newobj)
 
             return objlist
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Writes list of objects to a file in CSV format
+
+        Args:
+            list_objs (list): list of objects
+        """
+
+        clsname = cls.__name__
+        filename = "{}.csv".format(clsname)
+        rows = []
+        rectkeys = ["id", "width", "height", "x", "y"]
+        sqkeys = ["id", "size", "x", "y"]
+
+        if list_objs is not None:
+            for obj in list_objs:
+                row = []
+                objdct = obj.to_dictionary()
+                if clsname == "Rectangle":
+                    keys = rectkeys
+                elif clsname == "Square":
+                    keys = sqkeys
+                for key in keys:
+                    row.append(objdct[key])
+                rows.append(row)
+
+        with open(filename, mode="w", encoding="utf-8") as csvfile:
+            writer = csv.writer(csvfile)
+            for row in rows:
+                writer.writerow(row)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Reads CSV file into list of objects"""
+
+        clsname = cls.__name__
+        filename = "{}.csv".format(clsname)
+        rows = []
+        objlist = []
+        rectkeys = ["id", "width", "height", "x", "y"]
+        sqkeys = ["id", "size", "x", "y"]
+
+        if os.path.isfile(filename):
+            with open(filename, encoding="utf-8") as csvfile:
+                csvdata = csv.reader(csvfile)
+                rows = list(csvdata)
+            for row in rows:
+                i = 0
+                objdct = {}
+                if clsname == "Rectangle":
+                    keys = rectkeys
+                elif clsname == "Square":
+                    keys = sqkeys
+                for key in keys:
+                    objdct[key] = int(row[i])
+                    i += 1
+                newobj = cls.create(**objdct)
+                objlist.append(newobj)
+
+        return (objlist)
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Draws rectangles and squares on a new window"""
+
+        rect = list_rectangles[0].to_dictionary()
+
+        window = turtle.Screen()
+        turtle.speed(1)
+        turtle.setpos(rect["x"], rect["y"])
+        turtle.forward(rect["width"])
+        turtle.setheading(270)
+        turtle.forward(rect["height"])
+        turtle.setheading(180)
+        turtle.forward(rect["width"])
+        turtle.home()
+
+        turtle.exitonclick()
