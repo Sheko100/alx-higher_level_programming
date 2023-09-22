f430f76783172f01b968c065ccabb8b619c0308e
Shaker Sharabi <sheko100@icloud.com>
2023-09-22 16:01:36 +0300

    Fix size setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -39,8 +39,8 @@ class Square(Rectangle):
         elif value <= 0:
             raise ValueError("width must be > 0")
 
-        self.__width = value
-        self.__height = value
+        self.width = value
+        self.height = value
         self.__size = value
 
     def __str__(self):
