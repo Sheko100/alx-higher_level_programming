553193f0c27dda08fc791b9f8e24e3582b112ebe
Shaker Sharabi <sheko100@icloud.com>
2023-10-20 01:43:03 +0300

    Add exists user case

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates "user_0d_1" user and give it all privileges
-CREATE USER user_0d_1 IDENTIFIED BY "user_0d_1_pwd";
+CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY "user_0d_1_pwd";
 GRANT ALL
 ON *.*
 TO user_0d_1@localhost;
