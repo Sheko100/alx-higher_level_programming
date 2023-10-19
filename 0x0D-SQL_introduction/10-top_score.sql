b5f166518890dc2876ad69bcff64bf25489457bc
Shaker Sharabi <sheko100@icloud.com>
2023-10-19 18:46:54 +0300

    change to uppercase

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- lists the score and name columns of the second_table in desc order
-SELECT score, name from second_table ORDER BY score DESC
+SELECT score, name FROM second_table ORDER BY score DESC
