372ac338c20345e0c9f0e65e6ef7754532b4f696
Shaker Sharabi <sheko100@icloud.com>
2023-12-02 02:54:20 +0200

    Fix module executing on importing

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-# Fetches https://alx-intranet.hbtn.io/status
+"""Fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
-    html = res.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode("ascii")))
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+        html = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("ascii")))
+
