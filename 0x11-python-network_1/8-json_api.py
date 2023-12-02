28aaf77f853f0d3f8284e13b17828ce0f2df6158
Shaker Sharabi <sheko100@icloud.com>
2023-12-02 04:54:08 +0200

    Add module executing guard

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,13 +4,14 @@ a letter as a parameter
 """
 import requests
 from sys import argv
-q = argv[1] if len(argv) > 1 else ""
-res = requests.post("http://0.0.0.0:5000/search_user", json={'q': q})
-try:
-    json = res.json()
-    if len(json) == 0:
-        print("No result")
-    else:
-        print("[{}] {}".format(json.get("id"), json.get("name")))
-except requests.exceptions.JSONDecodeError as err:
-    print("Not a valid JSON")
+if __name__ == "__main__":
+    q = argv[1] if len(argv) > 1 else ""
+    res = requests.post("http://0.0.0.0:5000/search_user", json={'q': q})
+    try:
+        json = res.json()
+        if len(json) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(json.get("id"), json.get("name")))
+    except requests.exceptions.JSONDecodeError as err:
+        print("Not a valid JSON")
