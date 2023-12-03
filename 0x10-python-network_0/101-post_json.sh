c6b4599cc064e4067fc006d62c1eefd1ff2926e7
Shaker Sharabi <sheko100@icloud.com>
2023-12-04 01:41:13 +0200

    Fix command options

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays the body of the response
-curl -sX POST -H "Content-Type: application/json" $1 -d $2 
+curl -s -H "Content-Type: application/json" -H "Accept: application/json" $1 -d $2
