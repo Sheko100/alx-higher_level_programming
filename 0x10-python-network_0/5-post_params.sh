0653d43c451d4e0b6c2654bce139debc9238b28a
Shaker Sharabi <sheko100@icloud.com>
2023-12-04 01:13:24 +0200

    Fix command options

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request to the passed URL, and displays the body of the respons
-curl -sX POST -H "Content-Type: application/json" -d '{"email": "test@gmail.com", "subject": "I will always be here for PLD"}' $1
+curl -s -d "email=test@gmail.com" -d "subject=I will always be here for PLD" $1
