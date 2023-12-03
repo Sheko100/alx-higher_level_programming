181adf1545b0b3997b9e68dc67dde69801e9ea5d
Shaker Sharabi <sheko100@icloud.com>
2023-12-04 01:44:11 +0200

    Fix reading file option

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays the body of the response
-curl -s -H "Content-Type: application/json" -H "Accept: application/json" $1 -d $2
+curl -s -H "Content-Type: application/json" -H "Accept: application/json" "$1" -d @"$2"
