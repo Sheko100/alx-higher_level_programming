8aceea97dd48f32c6d731d88d8433603ac2be7dc
Shaker Sharabi <sheko100@icloud.com>
2024-01-04 03:02:53 +0200

    Add 0-readme.js file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,6 +4,6 @@ const { argv } = require('node:process');
 const fs = require('fs');
 
 fs.readFile(argv[2], 'utf-8', function (err, data) {
-    if (err) throw err;
-    console.log(data);
-  });
+  if (err) throw err;
+  console.log(data);
+});
