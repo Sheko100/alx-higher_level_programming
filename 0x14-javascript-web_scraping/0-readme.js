f93cc52d01e28da66db47d93c483447d5f34695a
Shaker Sharabi <sheko100@icloud.com>
2024-01-04 02:58:50 +0200

    Fix encoding

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -7,7 +7,7 @@ const argsCount = argv.length - 2;
 if (argsCount > 0) {
   const fs = require('fs');
 
-  fs.readFile(argv[2], 'utf8', function (err, data) {
+  fs.readFile(argv[2], 'utf-8', function (err, data) {
     if (err) throw err;
     console.log(data);
   });
