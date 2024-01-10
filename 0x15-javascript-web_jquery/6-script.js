5cf3b53f2b7408c1126cfc47c4184b4cf1477469
Shaker Sharabi <sheko100@icloud.com>
2024-01-10 20:27:46 +0200

    Fix coding style

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,5 +1,6 @@
 const header = $('header');
 $('DIV#update_header').on('click', function () {
-  if (header.text !== 'New Header!!!')
+  if (header.text !== 'New Header!!!') {
     header.text('New Header!!!');
+  }
 });
