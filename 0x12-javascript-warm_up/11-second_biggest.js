127332809e973936b7b88a2a262adfdb88446d9f
Shaker Sharabi <sheko100@icloud.com>
2024-02-15 04:31:48 +0200

    Add 11-second_biggest.js file

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+const argsCount = argv.length;
+let big = 0;
+let secBig = 0;
+let num = 0;
+
+if (argsCount > 3) {
+  big = parseInt(argv[2]);
+
+  for (let i = 3; i < argsCount; i++) {
+    num = parseInt(argv[i]);
+    if (big < num) {
+      big = num;
+    }
+  }
+  for (let i = 2; i < argsCount; i++) {
+    num = parseInt(argv[i]);
+    if (secBig < num && big !== num) {
+      secBig = num;
+    }
+  }
+  console.log(`secBig: ${secBig}`);
+} else {
+  console.log('0');
+}
