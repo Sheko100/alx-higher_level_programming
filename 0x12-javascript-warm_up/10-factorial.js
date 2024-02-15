f805bae1e3e2efeadc294dfe55df6a6e8d918d42
Shaker Sharabi <sheko100@icloud.com>
2024-02-15 03:21:50 +0200

    Add 10-factorial.js file

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const firstArg = process.argv[2];
+const n = parseInt(firstArg);
+
+function fac (num) {
+  let res = num;
+
+  if (num <= 0) {
+    return 0;
+  }
+  res += fac(num - 1);
+  return res;
+}
+
+if (isNaN(n)) {
+  console.log('1');
+} else {
+  console.log(fac(n));
+}
