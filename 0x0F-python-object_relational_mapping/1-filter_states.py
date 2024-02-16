0a35550af35043607eab5c109cdf38dab3d8b71e
Shaker Sharabi <sheko100@icloud.com>
2024-02-16 23:14:44 +0200

    Handle empty table

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -23,8 +23,11 @@ if __name__ == '__main__':
         cur.execute("""SELECT * FROM states WHERE name LIKE 'N%'
                 ORDER BY states.id ASC""")
         rows = cur.fetchall()
-        for row in rows:
-            print(row)
+        if len(rows) > 0:
+            for row in rows:
+                print(row)
+        else:
+            print(None)
 
         cur.close()
         db.close()
