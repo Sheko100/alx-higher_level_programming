4c5b218ae238194a0fb7806327015e1163171d86
Shaker Sharabi <sheko100@icloud.com>
2024-02-16 22:55:12 +0200

    Close connections

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
 
     db = MySQLdb.connect(
             host='localhost',
+            port=3306,
             user=username,
             password=pwd,
             database=db_name
@@ -22,3 +23,6 @@ if __name__ == '__main__':
     rows = cur.fetchall()
     for row in rows:
         print(row)
+
+    cur.close()
+    db.close()
