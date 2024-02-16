f4384bc02a48cbc4e360906672530753b59dfa77
Shaker Sharabi <sheko100@icloud.com>
2024-02-16 09:23:46 +0200

    Add 1-filter_states.py file

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module to select states with name started with 'N' from
+the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    username, pwd, db_name = argv[1], argv[2], argv[3]
+
+    db = MySQLdb.connect(
+            host='localhost',
+            user=username,
+            password=pwd,
+            database=db_name
+            )
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%'
+            ORDER BY states.id ASC""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
