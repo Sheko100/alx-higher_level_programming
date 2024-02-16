d71e33ee08fcaaa14d8daaaeefc637937c506a36
Shaker Sharabi <sheko100@icloud.com>
2024-02-16 23:56:29 +0200

    Add SQLi guard

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,16 +9,19 @@ from sys import argv
 if __name__ == '__main__':
     username, pwd, db_name = argv[1], argv[2], argv[3]
     state_name = argv[4]
-    query = """SELECT * FROM states WHERE name='{}'
-    ORDER BY states.id ASC""".format(state_name)
-    db = MySQLdb.connect(
-            host='localhost',
-            user=username,
-            password=pwd,
-            database=db_name
-            )
-    cur = db.cursor()
-    cur.execute(query)
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    if ';' not in state_name and '=' not in state_name:
+        query = """SELECT * FROM states WHERE name='{}'
+        ORDER BY states.id ASC""".format(state_name)
+        db = MySQLdb.connect(
+                host='localhost',
+                user=username,
+                password=pwd,
+                database=db_name
+                )
+        cur = db.cursor()
+        cur.execute(query)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
