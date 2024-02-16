c357dd0d93d0cc9048d1de7755c2795155045fe0
Shaker Sharabi <sheko100@icloud.com>
2024-02-16 09:38:24 +0200

    Add 2-my_filter_states.py file

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module to select a state row by name the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    username, pwd, db_name = argv[1], argv[2], argv[3]
+    state_name = argv[4]
+    query = """SELECT * FROM states WHERE name='{}'
+    ORDER BY states.id ASC""".format(state_name)
+    db = MySQLdb.connect(
+            host='localhost',
+            user=username,
+            password=pwd,
+            database=db_name
+            )
+    cur = db.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
