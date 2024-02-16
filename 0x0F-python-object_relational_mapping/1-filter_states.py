f3af4a793ad79b1795e14cf619f27f2b91133ae6
Shaker Sharabi <sheko100@icloud.com>
2024-02-16 23:02:22 +0200

    Add erros catching

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,19 +10,22 @@ from sys import argv
 if __name__ == '__main__':
     username, pwd, db_name = argv[1], argv[2], argv[3]
 
-    db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=username,
-            password=pwd,
-            database=db_name
-            )
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%'
-            ORDER BY states.id ASC""")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    try:
+        db = MySQLdb.connect(
+                host='localhost',
+                port=3306,
+                user=username,
+                password=pwd,
+                database=db_name
+                )
+        cur = db.cursor()
+        cur.execute("""SELECT * FROM states WHERE name LIKE 'N%'
+                ORDER BY states.id ASC""")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
-    cur.close()
-    db.close()
+        cur.close()
+        db.close()
+    except Exception as e:
+        print(e)
