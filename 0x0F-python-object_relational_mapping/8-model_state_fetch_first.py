10df4eb4d18cec6570dbb07ac70bbe9f1d2d28c0
Shaker Sharabi <sheko100@icloud.com>
2024-02-18 20:56:02 +0200

    Add 8-model_state_fetch_first.py file

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that selects all rows from the 'states' table using SQLAlchemy
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print('{}: {}'.format(state.id, state.name))
+    else:
+        print('Nothing')
