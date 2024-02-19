fcb46a4878145a14fdfac9a2f2be1de3f1b62f8d
Shaker Sharabi <sheko100@icloud.com>
2024-02-19 04:55:19 +0200

    Add 100-relationship_states_cities.py file

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that creates state: 'California' linked to city: 'San Francisco'
+using SQLAlchemy
+"""
+
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = State(name='California')
+    print(state.cities)
