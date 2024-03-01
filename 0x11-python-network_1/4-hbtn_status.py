4930dd22b4ac1fce9da97975e4260994471d30c7
Shaker Sharabi <sheko100@icloud.com>
2024-03-01 23:10:20 +0200

    Add 4-hbtn_status.py file

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that sends a HTTP request to a specific URL using urllib
+"""
+
+import requests
+
+if __name__ == "__main__":
+
+    url = "https://alx-intranet.hbtn.io/status"
+
+    res = requests.get(url)
+
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
