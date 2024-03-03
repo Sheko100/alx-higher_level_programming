9fbc0c1d10442f60fde53d68ba94d61ee5ae145c
Shaker Sharabi <sheko100@icloud.com>
2024-03-03 18:57:57 +0200

    Fix resource path

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# makes a request to 0.0.0.0:5000/catch_me using PUT method
-curl -LX PUT -b "user_id=989" http://0.0.0.0:5000/catch_me
+# makes a request to 0.0.0.0:5000/catch_me_3 using PUT method
+curl -X PUT http://0.0.0.0:5000/catch_me_3 -H "origin: School"
