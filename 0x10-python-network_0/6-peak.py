a9f8712540c8f35dc06ec21129aaa58cfc3fa2b9
Shaker Sharabi <sheko100@icloud.com>
2024-03-03 18:15:31 +0200

    Fix out of range index

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,10 +14,10 @@ def find_peak(list_of_integers):
     i = 1
 
     if length > 2:
-        while i < length - 1:
+        while i < length:
             left = list_of_integers[i-1]
             mid = list_of_integers[i]
-            right = list_of_integers[i+1]
+            right = list_of_integers[i+1] if i + 1 != length else 0
 
             if mid >= left and mid >= right:
                 return mid
