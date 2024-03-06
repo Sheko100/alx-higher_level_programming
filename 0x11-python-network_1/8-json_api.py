cdb8892f72031787ccd06bbfdc74d59cf9a41561
Shaker Sharabi <sheko100@icloud.com>
2024-03-06 03:42:56 +0200

    Add 8-json_api.py file

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module that sends a HTTP request to a specific URL using urllib
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    url = "http://0.0.0.0:5000/search_user"
+    data = {"q": ""}
+
+    if len(argv) > 1:
+        data['q'] = argv[1]
+
+    res = requests.post(url, data)
+    if res.status_code == 200:
+        try:
+            json = res.json()
+            if len(json) == 0:
+                print("No result")
+            else:
+                print("[{}], {}".format(json["id"], json["name"]))
+        except requests.exceptions.JSONDecodeError:
+            print("Not a valid JSON")
